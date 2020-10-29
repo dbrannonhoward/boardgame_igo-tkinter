ba15838537008d950dccb964834a36f791c6f6c7
dbrannonhoward <dbrannonhoward@gmail.com>
2020-10-29 19:08:06 -0400

    re-visiting project

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
-from tkinter import *
+import tkinter as tk
 
-root = Tk()
 
-# some canvas testing
+class ButtonBoard(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        # buttons
+        self.top_left = tk.Button(self)
+        self.top_right = tk.Button(self)
+        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
+        # pack
+        self.pack()
+        self.create_buttons()
 
-c = Canvas(root, height=500, width=500)
-c.pack()
+    def create_buttons(self):
+        self.top_left["text"] = "top left"
+        self.top_right["text"] = "top right"
 
-for step_size in range(15, 90, 15):
-    for i in range(5, 500, step_size):
-        if i % 2 == 0:
-            c.create_oval(i, i, i+step_size, i+step_size, fill="black", outline="white")
-        else:
-            c.create_oval(i, i, i+step_size, i+step_size, fill="white", outline="black")
+        self.top_left["command"] = self.top_left_button
+        self.top_right["command"] = self.top_right_button
 
-mainloop()
+        self.top_left.pack(side="left")
+        self.top_right.pack(side="right")
+        self.quit.pack(side="bottom")
+
+    def top_left_button(self):
+        print("top left button")
+
+    def top_right_button(self):
+        print("top right button")
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = ButtonBoard(master=root)
+    app.mainloop()
